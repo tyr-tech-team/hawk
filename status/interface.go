6ffda1853c56cf26df7edfccd44a45ed41dd8b70
max <maxr001@tyr-tech.com>
2020-11-16 16:41:31 +0800

    [add] add status new func Detail

--- status/interface.go
+++ status/interface.go
@@ -5,6 +5,7 @@ type Status interface {
 	Err() error
 	Error() string
 	String() string
+	Detail() []string
 	Status() Status
 	WithDetail(detail ...string) Status
 	SetServiceCode(serviceCode ServiceCode) Status
