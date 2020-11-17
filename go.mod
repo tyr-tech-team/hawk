903b401164e97cfd9b1e93e1c471d06810487709
max <maxr001@tyr-tech.com>
2020-11-17 17:47:58 +0800

    [add] add error in grpc middle traceid

--- go.mod
+++ go.mod
@@ -3,15 +3,21 @@ module github.com/tyr-tech-team/hawk
 go 1.15
 
 require (
-	github.com/axolotlteam/thunder v1.0.7
 	github.com/bwmarrin/snowflake v0.3.0
-	github.com/davecgh/go-spew v1.1.1
 	github.com/ghodss/yaml v1.0.0
 	github.com/gin-gonic/gin v1.6.3
+	github.com/gogo/protobuf v1.2.1 // indirect
+	github.com/google/btree v1.0.0 // indirect
+	github.com/grpc-ecosystem/go-grpc-middleware v1.0.0
 	github.com/hashicorp/consul/api v1.7.0
+	github.com/hashicorp/golang-lru v0.5.1 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/modern-go/reflect2 v1.0.1 // indirect
 	github.com/stretchr/testify v1.6.1
+	golang.org/x/crypto v0.0.0-20200323165209-0ec3e9974c59 // indirect
 	golang.org/x/net v0.0.0-20200421231249-e086a090c8fd // indirect
 	golang.org/x/sys v0.0.0-20200420163511-1957bb5e6d1f // indirect
 	google.golang.org/genproto v0.0.0-20200423170343-7949de9c1215 // indirect
 	google.golang.org/grpc v1.29.1
+	google.golang.org/protobuf v1.23.0 // indirect
 )
--- middleware/grpc/traceid.go
+++ middleware/grpc/traceid.go
@@ -3,14 +3,15 @@ package grpc
 import (
 	"context"
 
+	grpc_ctxtags "github.com/grpc-ecosystem/go-grpc-middleware/tags"
 	"github.com/tyr-tech-team/hawk/env"
 	"github.com/tyr-tech-team/hawk/trace"
 	"google.golang.org/grpc/metadata"
 )
 
 // TraceID - 讀取傳送過來的 metadata 中是否擁有TraceID
-func TraceID(ctx context.Context) context.Context {
-	rid := ""
+func TraceID(ctx context.Context) (context.Context, error) {
+	tid := ""
 	// 先取得是否有 metadata的存在
 	md, ok := metadata.FromIncomingContext(ctx)
 	if ok {
@@ -18,18 +19,21 @@ func TraceID(ctx context.Context) context.Context {
 		value := md.Get(env.TraceID.String())
 		if len(value) > 0 && value[0] != "" {
 			// 如果有RequestID 則覆用
-			rid = value[0]
+			tid = value[0]
 		}
 	}
 
-	if rid == "" {
-		rid = trace.NewTraceID()
+	if tid == "" {
+		tid = trace.NewTraceID()
 	}
 	// 將 TraceID 丟入 Conetext 之中
-	nctx := trace.SetTraceID(ctx, rid)
-	//
+	nctx := trace.SetTraceID(ctx, tid)
+
+	// TODO: 取代
+	grpc_ctxtags.Extract(ctx).Set(env.TraceID.String(), tid)
+
 	// 將 TraceID 附加到 OutgoingContext
-	return AppendTraceID(nctx, rid)
+	return AppendTraceID(nctx, tid), nil
 }
 
 // AppendTraceID -
